7326e6060e969228b6b3bbce04082195c492e49c
841723 <841723@unizar.es>
2024-07-19 19:32:05 +0200

    basic inversion, clearance

--- wolf-h3viewer-react/src/Map.jsx
+++ wolf-h3viewer-react/src/Map.jsx
@@ -2,22 +2,29 @@ import { useEffect, useState, useRef } from "react";
 
 export function Map() {
 
-    console.log("RENDERING MAP");
-
     const [map, setMap] = useState(undefined);
     var hexLayer = useRef(null);
-    const [selectedCellsIDs, setSelectedCellsIDs] = useState(["8182bffffffffff", "8182fffffffffff"]);
+    const [selectedCellsIDs, setSelectedCellsIDs] = useState([]);
+
+    const [showSelected, setShowSelected] = useState(true);
 
     useEffect(() => {
-        setMap(L.map('mapid'));
+        const m = L.map('mapid');
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            minZoom: 5,
+            maxZoom: 24,
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        }).addTo(m);
+
+        m.setView([40.5956, 0.5690], 17);
+        setMap(m);
     }, []);
 
     const GeoUtils = {
         EARTH_RADIUS_METERS: 6371000,
-
         radiansToDegrees: (r) => r * 180 / Math.PI,
         degreesToRadians: (d) => d * Math.PI / 180,
-
         getDistanceOnEarthInMeters: (lat1, lon1, lat2, lon2) => {
             const lat1Rad = GeoUtils.degreesToRadians(lat1);
             const lat2Rad = GeoUtils.degreesToRadians(lat2);
@@ -79,145 +86,112 @@ export function Map() {
     }
 
     function handleClickCell(h3id) {
-        if (selectedCellsIDs.includes(h3id)) {
-            setSelectedCellsIDs(selectedCellsIDs.filter((id) => id !== h3id));
-        }
-        else {
-            setSelectedCellsIDs((prev) => [...prev, h3id]);
-        }
-    }
-
-    useEffect(() => {
-        console.log("selectedCellsIDs changed to", selectedCellsIDs);
-        if (!map) return;
-        updateMapDisplay();
-    }, [selectedCellsIDs]);
-
-    function updateMapDisplay() {
-        // console.log("updateMapDisplay - hexLayer", hexLayer);
-        if (hexLayer.current) {
-            hexLayer.current.remove();
-        }
-
-        hexLayer.current = L.layerGroup().addTo(map);
-
-        const zoom = map.getZoom();
-        const currentH3Res = getH3ResForMapZoom(zoom);
-        const { _southWest: sw, _northEast: ne } = map.getBounds();
-
-        // TODO: change padding to be based on the map size
-        const padding = 0.1;
-        const extraPaddingLat = (ne.lat - sw.lat) * padding;
-        const extraPaddingLng = (ne.lng - sw.lng) * padding;
-
-
-        const boundsPolygon = [
-            [sw.lat - extraPaddingLat, sw.lng - extraPaddingLng],
-            [ne.lat + extraPaddingLat, sw.lng - extraPaddingLng],
-            [ne.lat + extraPaddingLat, ne.lng + extraPaddingLng],
-            [sw.lat - extraPaddingLat, ne.lng + extraPaddingLng],
-            [sw.lat - extraPaddingLat, sw.lng - extraPaddingLng],
-        ];
-
-        const h3s = h3.polygonToCells(boundsPolygon, currentH3Res);
-
-        for (const h3id of h3s) {
-
-            const polygonLayer = L.layerGroup()
-                .addTo(hexLayer.current);
-
-            if (h3id === "81823ffffffffff") {
-                console.log ("selectedCellsIDs.includes(81823ffffffffff);", selectedCellsIDs.includes(h3id));
-                console.log("selectedCellsIDs", selectedCellsIDs);
-            }
-            const isSelected = selectedCellsIDs.includes(h3id);
-
-            const style = isSelected ? { fillColor: "orange" } : {};
-
-            const h3Bounds = h3.cellToBoundary(h3id);
-            const averageEdgeLength = computeAverageEdgeLengthInMeters(h3Bounds);
-            const cellArea = h3.cellArea(h3id, "m2");
-
-            const tooltipText = `
-            Cell ID: <b>${h3id}</b>
-            <br />
-            Average edge length (m): <b>${averageEdgeLength.toLocaleString()}</b>
-            <br />
-            Cell area (m^2): <b>${cellArea.toLocaleString()}</b>
-            `;
-
-            const h3Polygon = L.polygon(h3BoundsToPolygon(h3Bounds), style)
-                .on('click', () => handleClickCell(h3id))
-                .bindTooltip(tooltipText)
-                .addTo(polygonLayer);
-
-            // less SVG, otherwise perf is bad
-            if (true) {
-                var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg");
-                svgElement.setAttribute('viewBox', "0 0 200 200");
-                svgElement.innerHTML = `<text x="20" y="70" class="h3Text">${h3id}</text>`;
-                var svgElementBounds = h3Polygon.getBounds();
-                L.svgOverlay(svgElement, svgElementBounds).addTo(polygonLayer);
-            }
-        }
-
-        // setHexLayer(hexLayerTmp);
-    }
-
-    useEffect(() => {
-        console.log("hexLayer changed to", hexLayer);
-    }, [hexLayer]);
-
-    function findH3() {
-        if (!h3.isValidCell(searchH3Id)) {
-            return;
-        }
-        const h3Boundary = h3.cellToBoundary(searchH3Id);
-
-        let bounds = undefined;
-
-        for ([lat, lng] of h3Boundary) {
-            if (bounds === undefined) {
-                bounds = new L.LatLngBounds([lat, lng], [lat, lng]);
+        setSelectedCellsIDs(prev => {
+            if (prev.includes(h3id)) {
+                return prev.filter((id) => id !== h3id);
             } else {
-                bounds.extend([lat, lng]);
+                return [...prev, h3id];
             }
-        }
-
-        map.fitBounds(bounds);
-
-        const newZoom = H3_RES_TO_ZOOM_CORRESPONDENCE[h3.getResolution(searchH3Id)];
-        map.setZoom(newZoom);
+        });
     }
 
     useEffect(() => {
         if (!map) return;
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            minZoom: 5,
-            maxNativeZoom: 19,
-            maxZoom: 24,
-            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-        }).addTo(map);
+        function updateMapDisplay() {
+            if (hexLayer.current) {
+                hexLayer.current.remove();
+            }
 
-        map.setView([0,0],5);
-        map.on("moveend", updateMapDisplay);
+            hexLayer.current = L.layerGroup().addTo(map);
+
+            const zoom = map.getZoom();
+            const currentH3Res = getH3ResForMapZoom(zoom);
+            const { _southWest: sw, _northEast: ne } = map.getBounds();
+
+            const padding = 0.1;
+            const extraPaddingLat = (ne.lat - sw.lat) * padding;
+            const extraPaddingLng = (ne.lng - sw.lng) * padding;
+
+            const boundsPolygon = [
+                [sw.lat - extraPaddingLat, sw.lng - extraPaddingLng],
+                [ne.lat + extraPaddingLat, sw.lng - extraPaddingLng],
+                [ne.lat + extraPaddingLat, ne.lng + extraPaddingLng],
+                [sw.lat - extraPaddingLat, ne.lng + extraPaddingLng],
+                [sw.lat - extraPaddingLat, sw.lng - extraPaddingLng],
+            ];
+
+            if (showSelected) {
+                for (const h3id of selectedCellsIDs) {
+                    const polygonLayer = L.layerGroup().addTo(hexLayer.current);
+    
+                    const h3Bounds = h3.cellToBoundary(h3id);
+    
+                    L.polygon(h3BoundsToPolygon(h3Bounds), { fillColor: "black", fillOpacity: 1, stroke: false })
+                        .addTo(polygonLayer);
+                }
+            }
+
+
+            const h3s = h3.polygonToCells(boundsPolygon, currentH3Res);
+
+            for (const h3id of h3s) {
+                const polygonLayer = L.layerGroup().addTo(hexLayer.current);
+
+                const h3Bounds = h3.cellToBoundary(h3id);
+
+                const style = !showSelected && !selectedCellsIDs.includes(h3id) ? { fillColor: "red", fillOpacity: 0.9, stroke: false } : { fillOpacity: 0.1, weight: 1 };
+
+                const h3Polygon = L.polygon(h3BoundsToPolygon(h3Bounds), style)
+                    .on('click', () => handleClickCell(h3id))
+                    .addTo(polygonLayer);
+
+                if (false) {
+                    var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                    svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+                    svgElement.setAttribute('viewBox', "0 0 200 200");
+                    svgElement.innerHTML = `<text x="20" y="70" class="h3Text">${h3id}</text>`;
+                    var svgElementBounds = h3Polygon.getBounds();
+                    L.svgOverlay(svgElement, svgElementBounds).addTo(polygonLayer);
+                }
+            }
+        }
+
+        if (!map) return;
+
+        const debouncedUpdateMapDisplay = debounce(updateMapDisplay, 200);
+
+        map.on("moveend", debouncedUpdateMapDisplay);
 
         updateMapDisplay();
 
         return () => {
-            map.off("moveend", updateMapDisplay);
-        }
-    }, [map]);
+            map.off("moveend", debouncedUpdateMapDisplay);
+        };
+    }, [map, selectedCellsIDs, showSelected]);
 
     return (
         <>
             <div id="mapid" style={{ height: "90vh", width: "100vw" }}></div>
             <button onClick={() => console.log(selectedCellsIDs)}>selectedCellsIDs</button>
-            <button onClick={() => console.log(selectedCellsIDs.includes("81823ffffffffff"))}>selectedCellsIDs.includes("81823ffffffffff")</button>
+            <button onClick={() => setShowSelected((prev) => !prev)}>invert colors</button>
+            <button onClick={() => setSelectedCellsIDs([])}>clear selected</button>
         </>
     );
 }
 
 export default Map;
+
+
+function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
