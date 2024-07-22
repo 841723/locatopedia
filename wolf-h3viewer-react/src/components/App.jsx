68655282620b370afea768c401e46284a530da39
841723 <841723@unizar.es>
2024-07-22 19:58:50 +0200

    add react router

--- wolf-h3viewer-react/src/components/App.jsx
+++ wolf-h3viewer-react/src/components/App.jsx
@@ -1,12 +1,25 @@
 import { Page } from "@/components/Page";
 import { Layout } from "@/layouts/Layout";
+import { useRoutes } from "react-router-dom";
+import Home from "@/components/Home";
 
 function App() {
-    return (
-        <Layout>
-            <Page />
-        </Layout>
-    );
+    const element = useRoutes([
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/wiki/:id",
+            element: <Page />,
+        },
+        {
+            path: '*',
+            element: <div className="text-9xl my-10">404 <br/>  Page not found</div>
+        }
+    ]);
+
+    return <Layout>{element}</Layout>;
 }
 
 export default App;
