dc96e948a29968f4ec6da793195bbbb5cf72409b
diego.uruena <118764694+841723@users.noreply.github.com>
2025-02-18 11:46:58 +0100

    Improve image handling in ArticleCard component by adding fallback for unsupported formats

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -25,19 +25,21 @@ export function ArticleCard({ item }) {
                 href={`/wiki/${item.hash}`}
                 className='flex flex-col gap-2 justify-between h-full'
             >
-                {
-                    img_url === null ? (
-                        <div className='flex items-center justify-center h-40 bg-gray-100 text-gray-500'>
-                            <Loader />
-                        </div>
-                    ) : (
+                {img_url === null ? (
+                    <div className='flex items-center justify-center h-40 bg-gray-100 text-gray-500'>
+                        <Loader />
+                        {item.img_url.split(".").pop() === "webp"
+                            ? `${BACKEND_API_URL}${item.img_url}`
+                            : item.img_url || "/sample.jpg"}
+                    </div>
+                ) : (
                     <img
                         src={img_url}
                         alt=''
                         className='object-cover h-40 grow rounded'
                     />
                 )}
-                
+
                 <div className='flex flex-col justify-end'>
                     <h2 className='text-[var(--color-secondary)] hover:underline hover:text-blue-900 text-lg font-semibold truncate'>
                         {item.title}
