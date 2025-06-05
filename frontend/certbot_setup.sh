679a9e1f0a63429fe1510d76b70de997ec875c76
diego.uruena <118764694+841723@users.noreply.github.com>
2025-06-05 19:35:48 +0200

    Docker set up. Make CORS_ORIGIN='http://localhost' in backend .env

--- frontend/certbot_setup.sh
+++ frontend/certbot_setup.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Configura Certbot si aún no se han generado certificados
-if [ ! -f "/etc/letsencrypt/live/locatopedia.com/fullchain.pem" ]; then
-    echo "Obteniendo certificados SSL con Certbot..."
-    certbot --nginx -d locatopedia.com -d www.locatopedia.com --non-interactive --agree-tos -m diegoraulroldan@gmail.com
-fi
+# # Configura Certbot si aún no se han generado certificados
+# if [ ! -f "/etc/letsencrypt/live/locatopedia.com/fullchain.pem" ]; then
+#     echo "Obteniendo certificados SSL con Certbot..."
+#     certbot --nginx -d locatopedia.com -d www.locatopedia.com --non-interactive --agree-tos -m diegoraulroldan@gmail.com
+# fi
 
-# Configura la renovación automática
-echo "Configurando la renovación automática de certificados..."
-echo "0 0 * * * certbot renew --quiet && nginx -s reload" | crontab -
+# # Configura la renovación automática
+# echo "Configurando la renovación automática de certificados..."
+# echo "0 0 * * * certbot renew --quiet && nginx -s reload" | crontab -
 
-echo "Certbot configurado. Iniciando Nginx..."
+# echo "Certbot configurado. Iniciando Nginx..."
